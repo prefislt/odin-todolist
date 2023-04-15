5329fc3b14151ddcef5d2a30ec7e77c6b7f5c914
Emilis Veliullajev <prefis@pm.me>
2023-04-15 16:28:04 +0300

    Add task editing

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -140,13 +140,13 @@ const dom = (() => {
                                     <span id="todoText" class="font-bold">${projects.projectsList[i].tasks[l].title}</span>
                                     <span id="todoDesc" class="text-xs">${projects.projectsList[i].tasks[l].description}</span>
                                 </div>
-                                <div id="actionButtons" class="flex flex-row gap-2" data-projectindex="${i}" data-taskindex="${l}">
-                                    <button class="btn" id="todoEdit">
-                                        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 stroke-current"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title></title> <g id="Complete"> <g id="edit"> <g> <path d="M20,16v4a2,2,0,0,1-2,2H4a2,2,0,0,1-2-2V6A2,2,0,0,1,4,4H8" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path> <polygon fill="none" points="12.5 15.8 22 6.2 17.8 2 8.3 11.5 8 16 12.5 15.8" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></polygon> </g> </g> </g> </g></svg>
-                                    </button>
-                                    <button class="btn" id="todoDelete" data-projectindex="${i}" data-taskindex="${l}">
+                                <div id="actionButtons" class="flex flex-row gap-2">
+                                <label for="popup" id="editTaskButton" class="btn" data-projectindex="${i}" data-taskindex="${l}">
+                                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 stroke-current"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title></title> <g id="Complete"> <g id="edit"> <g> <path d="M20,16v4a2,2,0,0,1-2,2H4a2,2,0,0,1-2-2V6A2,2,0,0,1,4,4H8" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path> <polygon fill="none" points="12.5 15.8 22 6.2 17.8 2 8.3 11.5 8 16 12.5 15.8" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></polygon> </g> </g> </g> </g></svg>
+                                </label>
+                                <button class="btn" id="todoDelete" data-projectindex="${i}" data-taskindex="${l}">
                                     <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 stroke-current"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M10 12V17" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M14 12V17" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M4 7H20" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M6 10V18C6 19.6569 7.34315 21 9 21H15C16.6569 21 18 19.6569 18 18V10" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M9 5C9 3.89543 9.89543 3 11 3H13C14.1046 3 15 3.89543 15 5V7H9V5Z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
-                                    </button>
+                                </button>
                                 </div>
                             </div>
                     `;
@@ -166,10 +166,10 @@ const dom = (() => {
                                 <span id="todoText" class="font-bold">${projects.projectsList[index].tasks[i].title}</span>
                                 <span id="todoDesc" class="text-xs">${projects.projectsList[index].tasks[i].description}</span>
                             </div>
-                            <div id="actionButtons" class="flex flex-row gap-2" data-projectindex="${index}" data-taskindex="${i}">
-                                <button class="btn" id="todoEdit">
+                            <div id="actionButtons" class="flex flex-row gap-2">
+                                <label for="popup" id="editTaskButton" class="btn" data-projectindex="${index}" data-taskindex="${i}">
                                     <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 stroke-current"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title></title> <g id="Complete"> <g id="edit"> <g> <path d="M20,16v4a2,2,0,0,1-2,2H4a2,2,0,0,1-2-2V6A2,2,0,0,1,4,4H8" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path> <polygon fill="none" points="12.5 15.8 22 6.2 17.8 2 8.3 11.5 8 16 12.5 15.8" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></polygon> </g> </g> </g> </g></svg>
-                                </button>
+                                </label>
                                 <button class="btn" id="todoDelete" data-projectindex="${index}" data-taskindex="${i}">
                                 <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 stroke-current"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M10 12V17" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M14 12V17" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M4 7H20" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M6 10V18C6 19.6569 7.34315 21 9 21H15C16.6569 21 18 19.6569 18 18V10" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M9 5C9 3.89543 9.89543 3 11 3H13C14.1046 3 15 3.89543 15 5V7H9V5Z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
                                 </button>
@@ -179,9 +179,21 @@ const dom = (() => {
       }
     }
 
-    let buttons = document.querySelectorAll("#todoDelete").length;
+    // Add event listener to all edit buttons on tasks
+    let editButtons = document.querySelectorAll("#editTaskButton").length;
+    for (let i = 0; i < editButtons; i++) {
+      document
+        .querySelectorAll("#editTaskButton")
+        [i].addEventListener("click", (e) => {
+          const taskIndex = e.currentTarget.dataset.taskindex;
+          const projectIndex = e.currentTarget.dataset.projectindex;
+          dom.editTask(taskIndex, projectIndex);
+        });
+    }
 
-    for (let i = 0; i < buttons; i++) {
+    // Add event listener to all delete buttons on tasks
+    let deleteButtons = document.querySelectorAll("#todoDelete").length;
+    for (let i = 0; i < deleteButtons; i++) {
       document
         .querySelectorAll("#todoDelete")
         [i].addEventListener("click", (e) => {
@@ -193,9 +205,58 @@ const dom = (() => {
     }
   }
 
+  function editTask(taskIndex, projectIndex) {
+    document.querySelector("#popupContent").innerHTML = /*html*/ `
+            <input type="checkbox" id="popup" class="modal-toggle" />
+                <label for="popup" class="modal cursor-pointer">
+                    <label class="modal-box relative">
+                        <span>EDIT TASK</span>
+                        <div id="editTaskContent" class="flex flex-col gap-2 justify-center items-center p-4 max-w-lg">
+                            <input class="input input-bordered w-full" id="inputTaskNameEdit" type="text" placeholder="Task name" value="${projects.projectsList[projectIndex].tasks[taskIndex].title}">
+                            <input class="input input-bordered w-full" id="inputDescriptionEdit" type="text" placeholder="Description" value="${projects.projectsList[projectIndex].tasks[taskIndex].description}">
+                            <input class="input input-bordered w-full" id="inputDateEdit" type="date" placeholder="Select date" value="${projects.projectsList[projectIndex].tasks[taskIndex].date}">
+                            <select class="select select-bordered w-full" id="inputPriorityEdit">
+                                <option disabled>Priority</option>
+                                <option>Low</option>
+                                <option>Medium</option>
+                                <option>High</option>
+                            </select>
+                            <button class="btn btn-primary" id="submitTaskEdit">Edit task</button>
+                            <div class="alert alert-error shadow-lg">
+                                <div>
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                                    <span>Error! Just example, for now it's doing nothing. So don't worry :)</span>
+                                </div>
+                            </div>
+                        </div>
+                    </label> 
+                </label>
+        `;
+
+    document.querySelector("#submitTaskEdit").addEventListener("click", () => {
+      const inputTaskName = document.querySelector("#inputTaskNameEdit").value;
+      const inputDescription = document.querySelector(
+        "#inputDescriptionEdit"
+      ).value;
+      const inputDate = document.querySelector("#inputDateEdit").value;
+      const inputPriority = document.querySelector("#inputPriorityEdit").value;
+
+      projects.projectsList[projectIndex].tasks[taskIndex] = {
+        title: inputTaskName,
+        description: inputDescription,
+        date: inputDate,
+        priority: inputPriority,
+        checked: false,
+      };
+
+      dom.renderTasks(document.querySelector("#todolist").dataset.projectindex);
+    });
+  }
+
   return {
     renderProjects,
     renderTasks,
+    editTask,
   };
 })();
 
--- src/modules/tasks.js
+++ src/modules/tasks.js
@@ -2,31 +2,31 @@ import dom from "./dom";
 import projects from "./projects";
 
 const tasks = (() => {
-	class CreateTask {
-		constructor(title, description, date, priority) {
-			this.title = title;
-			this.description = description;
-			this.date = date;
-			this.priority = priority;
-			this.checked = false;
-		}
-	}
+  class CreateTask {
+    constructor(title, description, date, priority) {
+      this.title = title;
+      this.description = description;
+      this.date = date;
+      this.priority = priority;
+      this.checked = false;
+    }
+  }
 
-	function addTask(title, description, date, priority, projectIndex) {
-		const newTask = new CreateTask(title, description, date, priority);
-		projects.projectsList[projectIndex].tasks.push(newTask);
-		dom.renderTasks(projectIndex);
-	}
+  function addTask(title, description, date, priority, projectIndex) {
+    const newTask = new CreateTask(title, description, date, priority);
+    projects.projectsList[projectIndex].tasks.push(newTask);
+    dom.renderTasks(document.querySelector("#todolist").dataset.projectindex);
+  }
 
-	function removeTask(projectIndex, taskIndex) {
-		projects.projectsList[projectIndex].tasks.splice(taskIndex, 1);
-		dom.renderTasks(projectIndex);
-	}
+  function removeTask(projectIndex, taskIndex) {
+    projects.projectsList[projectIndex].tasks.splice(taskIndex, 1);
+    dom.renderTasks(document.querySelector("#todolist").dataset.projectindex);
+  }
 
-	return {
-		addTask,
-		removeTask,
-	};
+  return {
+    addTask,
+    removeTask,
+  };
 })();
 
 export default tasks;
