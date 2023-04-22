c80441647b1db4450129c20f19a56418a5c4f95c
Emilis Veliullajev <prefis@pm.me>
2023-04-22 14:03:43 +0300

    Make task buttons a bit smaller

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -158,10 +158,10 @@ const dom = (() => {
             </div>
           </div>
           <div id="actionButtons" class="flex flex-col gap-2">
-            <label for="popup" id="todoDelete" class="btn btn-sm btn-ghost" data-projectindex="${projectIndex}" data-taskindex="${taskIndex}">
+            <label for="popup" id="todoDelete" class="btn btn-xs btn-ghost" data-projectindex="${projectIndex}" data-taskindex="${taskIndex}">
                 <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-4 stroke-primary-content"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M10 12V17" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M14 12V17" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M4 7H20" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M6 10V18C6 19.6569 7.34315 21 9 21H15C16.6569 21 18 19.6569 18 18V10" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M9 5C9 3.89543 9.89543 3 11 3H13C14.1046 3 15 3.89543 15 5V7H9V5Z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
             </label>
-            <label for="popup" id="editTaskButton" class="btn btn-sm btn-ghost" data-projectindex="${projectIndex}" data-taskindex="${taskIndex}">
+            <label for="popup" id="editTaskButton" class="btn btn-xs btn-ghost" data-projectindex="${projectIndex}" data-taskindex="${taskIndex}">
                 <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="w-4 stroke-primary-content"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title></title> <g id="Complete"> <g id="edit"> <g> <path d="M20,16v4a2,2,0,0,1-2,2H4a2,2,0,0,1-2-2V6A2,2,0,0,1,4,4H8" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path> <polygon fill="none" points="12.5 15.8 22 6.2 17.8 2 8.3 11.5 8 16 12.5 15.8" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></polygon> </g> </g> </g> </g></svg>
             </label>
           </div>
